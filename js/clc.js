15e70e1cff6e6274b3950304e9477acbc7402d67
Luke Garwood <luke_gar@hotmail.com>
2016-03-27 21:12:58 -0400

    switched to a for loop with an array in the clc.js

--- js/clc.js
+++ js/clc.js
@@ -1,63 +1,46 @@
 //-----Particle API stuff
 
-	  //Defines a new particle object
-		  var particle = new Particle(); 
+//Defines a new particle object
+var particle = new Particle(); 
 		  
-		  //Counts the number of events received - Each count represents 20 people 
-		  var count = 0;
+//Counts the number of events received - Each count represents 20 people 
+var count = 0;
+//Array containing all our images with html code
+var imageArray =['<img id="sit1" class= "img-responsive" src="img/sit1.png">','<img id="sit2" class= "img-responsive" src="img/sit2.png">','<img id="sit3" class= "img-responsive" src="img/sit3.png">','<img id="sitclap" class= "img-responsive" src="img/sitclap.png">',]
 		  
-	      particle.getEventStream({ deviceId: '28001c001647343337363432', name:'Motion-Detected', auth: '057fe60aff7c3d43c02d35f072514251ddc2e8c1'   }).then(function(stream) {
+	      
+particle.getEventStream({ deviceId: '28001c001647343337363432', name:'Motion-Detected', auth: '057fe60aff7c3d43c02d35f072514251ddc2e8c1'   }).then(function(stream) {
           stream.on('Motion-Detected', function(data) {
-        
-		     if (data.name=="Motion-Detected"){
+                if (data.name=="Motion-Detected"){
                  var image;
-                 switch(count){
-                    case 0: image = '<img id="sit3" class= "img-responsive" src="img/sit3.png">';
-                    break;
-                         
-                    case 1: image = '<img id="sitclap" class= "img-responsive" src="img/sitclap.png">';
-                    break;
-                         
-                    case 2: image = '<img id="sit1" class= "img-responsive" src="img/sit1.png">';
-                    break;
-                         
-                    case 3: image = '<img id="sit2" class= "img-responsive" src="img/sit2.png">';
-                    break;
-                         
-                    default: console.log("no image");
-                }
-
-                 //adding images into the ghostImage div
+                    for(i=0;i<=imageArray.length; i++){
+                        if(i==count){
+                        imageArray[i] = image
+                        }
+                    }
+                //adding images into the ghostImage div
                  $('#ghostImage').append(image);
-                 
-                 
-				 count = count+1;
+                //increasing count
+                  count = count+1;
              }
-	 
-  });
+        });
 });
 
 
 ////-----Images showing on click for testing
-//
+
 //var imageCounter = 0;
 //
 //$("body").click(function(){
-//  
-//switch(imageCounter){
-//    case 0: var image = '<img id="sit3" class= "img-responsive" src="img/sit3.png">';
-//    break;
-//    case 1: var image = '<img id="sitclap" class= "img-responsive" src="img/sitclap.png">';
-//    break;
-//    case 2: var image = '<img id="sit1" class= "img-responsive" src="img/sit1.png">';
-//    break;
-//    case 3: var image = '<img id="sit2" class= "img-responsive" src="img/sit2.png">';
-//    break;
-//    default: console.log("no image");
+//  var image;
+//for(i=0;i<=imageArray.length; i++){
+//    if(i==count){
+//    image = imageArray[i];
+//    }
 //}
 //   
 //    $('#ghostImage').append(image);
 //
-//    imageCounter += 1;
+//    count += 1;
 //})
 
